1ee5f9eef35ab8335906c1cbfc3c2fbce3bff631
GlennVinck <vinck_glenn@hotmail.com>
2023-12-11 23:06:04 +0100

    fix: resolved an issue with the width of topbar exceeding view width

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,7 +1,9 @@
 <script setup></script>
 
 <template>
-  <div class="w-screen h-[64px] bg-white"></div>
+  <div class="w-full h-[64px] bg-white">
+    <h1>Topbar</h1>
+  </div>
 </template>
 
 <style scoped></style>
