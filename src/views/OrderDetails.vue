73d3269c001c71772731b86eb2af7b58b5510a42
GlennVinck <vinck_glenn@hotmail.com>
2023-12-12 14:31:45 +0100

    feat: back button on order details page

--- src/views/OrderDetails.vue
+++ src/views/OrderDetails.vue
@@ -1,9 +1,43 @@
-<script setup></script>
+<script setup>
+import Sidebar from "../components/Sidebar.vue";
+import Topbar from "../components/Topbar.vue";
+import { ref, watchEffect } from "vue";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const previousRoute = ref(null);
+
+// Watch for route changes
+watchEffect(() => {
+  previousRoute.value = router.currentRoute.value;
+});
+</script>
 
 <template>
-  <div>
-    <h1>Topbar</h1>
+  <div class="w-screen h-screen flex bg-neutral-200">
+    <Sidebar />
+    <div class="w-full">
+      <Topbar />
+      <div class="p-8">
+        <button @click="goBack" class="text-blue-500 hover:underline">
+          Back
+        </button>
+        <h1 class="text-2xl font-semibold mb-2">Order</h1>
+      </div>
+    </div>
   </div>
 </template>
+
+<script>
+export default {
+  methods: {
+    goBack() {
+      // Use the previous route information to determine where to go back
+      const backRoute = this.previousRoute?.value?.name || "/";
+      this.$router.push(backRoute);
+    },
+  },
+};
+</script>
 
 <style scoped></style>
