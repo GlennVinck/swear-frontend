49500cd737a567a72331aee772833f749dd9f236
GlennVinck <vinck_glenn@hotmail.com>
2023-12-12 15:30:55 +0100

    feat: search input field in topbar

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,8 +1,21 @@
 <script setup></script>
 
 <template>
-  <div class="w-full h-[64px] bg-white">
-    <h1>Topbar</h1>
+  <div
+    class="w-full h-[64px] p-2 bg-white flex row items-center justify-between"
+  >
+    <div class="flex items-center relative ml-2">
+      <input
+        type="text"
+        placeholder="Search"
+        class="h-[32px] min-w-[320px] px-4 bg-stone-100 border-none rounded focus:outline-none placeholder:text-sm placeholder:text-neutral-400 focus:ring-1 focus:ring-primary-accent"
+      />
+      <img
+        src="../assets/icons/search.svg"
+        alt="Search Icon"
+        class="absolute right-4"
+      />
+    </div>
   </div>
 </template>
 
