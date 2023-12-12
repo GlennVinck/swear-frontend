fd9ca25bba739bcf2a7696408ac97ad2096f9369
Wolf <wolfverelst@gmail.com>
2023-12-12 20:58:51 +0100

    feat(Orders.vue): Show all orders

--- src/views/Orders.vue
+++ src/views/Orders.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Sidebar from "../components/Sidebar.vue";
 import Topbar from "../components/Topbar.vue";
+import OrderTable from "../components/OrderTable.vue";
 </script>
 
 <template>
@@ -8,8 +9,15 @@ import Topbar from "../components/Topbar.vue";
     <Sidebar />
     <div class="w-full">
       <Topbar />
-      <div>
-        <h1>All Orders</h1>
+      <div class="p-8">
+        <h1 class="text-2xl font-semibold mb-4">Orders</h1>
+        <div class="w-fit p-4 pr-0 my-4">
+          <!-- You can add any other buttons or filters specific to the Orders page here -->
+        </div>
+        <div class="w-screeen max-w-7xl p-4 pb-0 my-4">
+          <OrderTable />
+          <!-- You can pass data or props to OrderTable component if needed -->
+        </div>
       </div>
     </div>
   </div>
