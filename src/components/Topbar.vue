ea51d0f09af7316327d175ba0477d24e4818de5e
Wolf <wolfverelst@gmail.com>
2023-12-13 16:39:09 +0100

    feat(Topbar.vue): add Account Dropdown Menu

--- src/components/Topbar.vue
+++ src/components/Topbar.vue
@@ -1,4 +1,12 @@
-<script setup></script>
+<script setup>
+import { ref } from 'vue';
+
+const showDropdown = ref(false);
+
+const toggleDropdown = () => {
+  showDropdown.value = !showDropdown.value;
+};
+</script>
 
 <template>
   <div
@@ -16,12 +24,21 @@
         class="absolute right-4"
       />
     </div>
-    <div class="flex flex-row gap-4 items-center justify-center mr-4">
+    <div class="flex flex-row gap-4 items-center justify-center mr-4 cursor-pointer relative" @click="toggleDropdown">
       <img
         src="../assets/profilepic.jpg"
         alt="profile picture"
         class="w-[32px] h-[32px] rounded-full"
       />
+
+      <div :class="{ 'dropdown-show': showDropdown }" class="dropdown">
+          <!-- Dropdown content -->
+          <div class="dropdown-item">Account</div>
+          <div class="dropdown-item">Settings</div>
+          <div class="dropdown-item">Sign out</div>
+          <!-- Add other useful links here -->
+        </div>
+
       <div class="flex flex-col">
         <span class="text-sm font-semibold">Joris Hens</span>
         <span class="text-xs text-neutral-400">Admin</span>
@@ -35,4 +52,30 @@
   </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.dropdown {
+  position: absolute;
+  top: 100%;
+  right: 0;
+  background-color: white;
+  border: 1px solid #ccc;
+  border-radius: 4px;
+  box-shadow: 0 2px 4px rgba(0, 0, 0, 0.1);
+  display: none;
+  z-index: 1000;
+}
+
+.dropdown-item {
+  padding: 8px;
+  cursor: pointer;
+}
+
+.dropdown-item:hover {
+  background-color: #f0f0f0;
+}
+
+/* Show the dropdown when the showDropdown value is true */
+.dropdown-show {
+  display: block;
+}
+</style>
